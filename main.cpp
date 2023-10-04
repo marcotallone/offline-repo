997c8cb8f442ca45cbfd3916998a4046a0efa8b4
marcotallone <marco.tallone@studenti.units.it>
2023-10-04 23:27:57 +0200

    Changes made

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main(){
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello people from world!" << std::endl;
     int c=10;
+    std::cout << "Hello people from the other world!" << std::endl;
+    int a = 25;
 
     return 0;
 }
